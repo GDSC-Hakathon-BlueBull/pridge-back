5dd2c0f7e0414be9d62688c8ab0a82b42cd71aea
JaeGun Cho <whworjs777@gmail.com>
2023-02-11 19:45:36 +0900

    [Feat] Setup logger, swagger, validator, prisma (#2)
    * add prismaService
    
    * setup pipes and swagger
    
    * setup logger
    
    * import commonModule
    
    * setup configMoudle, env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { CommonModule } from './common/common.module';
+import { configModule } from './app/modules/config.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule, configModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // class validator 세팅
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      skipUndefinedProperties: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  // swagger 세팅
+  const config = new DocumentBuilder()
+    .setTitle('Pridge Server')
+    .setDescription('Pridge API description')
+    .setVersion('1.0')
+    .addTag('Pridge')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
