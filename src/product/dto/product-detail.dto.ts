45c1603b3d41cd27dc11e660a3e3caf5a20ce7af
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2023-02-12 01:40:27 +0900

    [feature] productList

--- src/product/dto/product-detail.dto.ts
+++ src/product/dto/product-detail.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class ProductDto {
+export class ProductDetailDto {
   @ApiProperty({ type: Number, description: 'Product ID' })
   id!: number;
 
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../common/services/prisma.service';
-import { Product } from '@prisma/client';
-import { ProductDto } from './dto/product.dto';
+import { ProductDetailDto } from './dto/product-detail.dto';
+import { ProductListDto } from './dto/productlist.dto';
 
+//product 상세
 @Injectable()
 export class ProductService {
   constructor(private readonly prisma: PrismaService) {}
-  async getProductById(id: number): Promise<ProductDto> {
+  async getProductById(id: number): Promise<ProductDetailDto> {
     const product = await this.prisma.product.findUnique({
       where: {
         id,
@@ -19,4 +20,20 @@ export class ProductService {
 
     return product;
   }
+
+  async getProductList(): Promise<ProductListDto> {
+    const products = await this.prisma.product.findMany({
+      select:
+      {
+        id: true,
+        name: true,
+        price: true,
+        photo: true,
+        size: true,
+      }
+     });
+    return {products};
+  }
 }
+
+
