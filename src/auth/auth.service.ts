bf5f352790f69a99ec7cecf69908085d67d06005
worjs <whworjs777@gmail.com>
2023-02-12 00:16:46 +0900

    add service method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { AuthRepository } from './auth.repository';
+import { RegisterConsumerPayload } from './payload/register-consumer.payload';
+import { RegisterDonatorPayload } from './payload/register-donator.payload';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly authRepository: AuthRepository) {}
+
+  async registerConsumer(payload: RegisterConsumerPayload): Promise<void> {
+    return;
+  }
+
+  async registerDonator(payload: RegisterDonatorPayload): Promise<void> {
+    return;
+  }
+
+  async login(token: string): Promise<void> {
+    return;
+  }
 }
