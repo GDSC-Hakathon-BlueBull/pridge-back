7cade8c02c1f8f516ab91837d62e823a986c33a7
worjs <whworjs777@gmail.com>
2023-02-11 23:26:00 +0900

    setup auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoggerMiddleware } from '../common/middlewares/logger.middleware';
 import { CommonModule } from '../common/common.module';
 import { configModule } from './modules/config.module';
 import { ProductModule } from '../product/product.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [CommonModule, configModule, ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommonModule, configModule, ProductModule, AuthModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
