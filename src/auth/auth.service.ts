785f238ec9d4b0c05604c672c17bc0d725dc4d9e
worjs <whworjs777@gmail.com>
2023-02-12 00:14:50 +0900

    add authRepository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { AuthRepository } from './auth.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly authRepository: AuthRepository) {}
+}
