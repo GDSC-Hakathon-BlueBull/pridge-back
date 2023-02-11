48f6b18671217bc5e590f7d24d8076ba38aacec1
JaeGun Cho <whworjs777@gmail.com>
2023-02-11 23:19:26 +0900

    product module & make getProductById (#4)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerMiddleware } from './common/middlewares/logger.middleware';
-import { CommonModule } from './common/common.module';
-import { configModule } from './app/modules/config.module';
+import { LoggerMiddleware } from '../common/middlewares/logger.middleware';
+import { CommonModule } from '../common/common.module';
+import { configModule } from './modules/config.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
-  imports: [CommonModule, configModule],
+  imports: [CommonModule, configModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
