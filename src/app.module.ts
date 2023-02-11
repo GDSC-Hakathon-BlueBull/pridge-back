301e27ca37a7c06936d7aba3eb4161643cc24228
worjs <whworjs777@gmail.com>
2023-02-11 19:41:30 +0900

    setup configMoudle, env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { CommonModule } from './common/common.module';
+import { configModule } from './app/modules/config.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, configModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/app/modules/config.module.ts
@@ -0,0 +1,16 @@
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+export const configModule = ConfigModule.forRoot({
+  isGlobal: true,
+  envFilePath: `.${process.env.NODE_ENV}.env`,
+  validationSchema: Joi.object({
+    NODE_ENV: Joi.string().valid('dev', 'prod', 'local'),
+    POSTGRES_USER: Joi.string().required(),
+    POSTGRES_PASSWORD: Joi.string().required(),
+    POSTGRES_DB: Joi.string().required(),
+    DATABASE_URL: Joi.string().required(),
+    JWT_ACCESS_TOKEN_EXPIRE: Joi.string().required(),
+    JWT_SECRET_KEY: Joi.string().required(),
+  }),
+});
